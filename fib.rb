c1035a0f9524820014248c3d89f8696829bd3b31
Amine TheDream <aminethedream@Amines-MacBook-Air.local>
2013-12-13 15:07:29 -0500

    awesome amine

--- fib.rb
+++ fib.rb
@@ -2,5 +2,9 @@
 # create a method that returns the nth number of the fibonacci sequence
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 def fibo_finder(n)
-  # code goes here
+ if n < 2
+ 	n
+ else
+ 	fibo_finder(n-1) + fibo_finder(n-2)
+ end
 end
